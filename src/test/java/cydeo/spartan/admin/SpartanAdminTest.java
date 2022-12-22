4607d29c0a38ff72157084e554987f073d27dcdb
mehmetCt <mehmet@cybertekschool.com>
2022-12-22 15:40:43 -0500

    Initial Serenity

--- src/test/java/cydeo/spartan/admin/SpartanAdminTest.java
+++ src/test/java/cydeo/spartan/admin/SpartanAdminTest.java
@@ -2,6 +2,7 @@ package cydeo.spartan.admin;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.junit5.SerenityTest;
 import net.serenitybdd.rest.SerenityRest;
@@ -10,6 +11,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+
+
 @SerenityTest
 public class SpartanAdminTest {
 
@@ -23,9 +27,14 @@ public class SpartanAdminTest {
     @Test
     public void test1() {
 
-        given().accept(ContentType.JSON)
-                .auth().basic("admin","admin").
-        when().get("/api/spartans").prettyPeek();
+        Response response = given().accept(ContentType.JSON)
+                .auth().basic("admin", "admin").
+                when().get("/api/spartans").prettyPeek();
+
+
+        System.out.println("response.path(\"id[0]\") = " + response.path("id[0]"));
+
+        System.out.println("response.statusCode() = " + response.statusCode());
 
     }
 
@@ -37,11 +46,20 @@ public class SpartanAdminTest {
                 .pathParam("id",45)
         .when().get("/api/spartans/{id}").prettyPeek();
 
+
+        // lastResponse --> response --> Serenity Rest will generate after sending request and store resposne information
+        // without saving in a variable
+        lastResponse();
+
+        System.out.println("lastResponse().statusCode() = " + lastResponse().statusCode());
+
+        //RESPONSE
+        System.out.println("lastResponse().path(\"id\") = " + lastResponse().path("id"));
+
+        //JSONPATH
+        System.out.println("lastResponse().jsonPath().getInt(\"id\") = " + lastResponse().jsonPath().getInt("id"));
+
+
     }
-
-
-
-
-
 
 }
